a65f1a65b684a9f792ba8cf85b8a3d6346791261
TPKato <44572590+TPKato@users.noreply.github.com>
2021-05-22 21:52:31 +0200

    update for GIMP 2.10

--- luminous.scm
+++ luminous.scm
@@ -1,52 +1,105 @@
-;  luminous-logo
-;  Create a luminous text effect
-;  (Based on alien-glow-logo.scm by Spencer Kimball)
-;  $Id: luminous.scm,v 1.2 1998/08/03 07:32:00 p-katoh Exp $
+;;; luminous-logo
+;;; Create a luminous text effect
+;;; (Based on alien-glow-logo.scm by Spencer Kimball)
 
-(define (script-fu-luminous-logo text size font glow-color grow-strength background-color)
-  (let ((old-fg (car (gimp-palette-get-foreground)))
-	(old-bg (car (gimp-palette-get-background))))
-    (gimp-palette-set-foreground '(255 255 255))
-    (let* ((img (car (gimp-image-new 256 256 RGB)))
-	   (border (/ size 4))
-	   (grow (/ size grow-strength))
-	   (feather (/ size 4))
-	   (text-layer (car (gimp-text img -1 0 0 text border TRUE size PIXELS "*" font "*" "*" "*" "*")))
-	   (width (car (gimp-drawable-width text-layer)))
-	   (height (car (gimp-drawable-height text-layer)))
-	   (bg-layer (car (gimp-layer-new img width height RGB_IMAGE "Background" 100 NORMAL)))
-	   (glow-layer (car (gimp-layer-new img width height RGBA_IMAGE "Luminous Glow" 100 NORMAL))))
-      (gimp-image-disable-undo img)
-      (gimp-image-resize img width height 0 0)
-      (gimp-image-add-layer img bg-layer 1)
-      (gimp-image-add-layer img glow-layer 1)
-      (gimp-layer-set-preserve-trans text-layer TRUE)
-      (gimp-palette-set-background background-color)
-      (gimp-edit-fill img bg-layer)
-      (gimp-edit-clear img glow-layer)
-      (gimp-selection-layer-alpha img text-layer)
-      (gimp-selection-grow img grow)
-      (gimp-selection-feather img feather)
-      (gimp-palette-set-background glow-color)
-      (gimp-edit-fill img glow-layer)
-      (gimp-selection-none img)
-      (gimp-layer-set-name text-layer text)
-      (gimp-palette-set-background old-bg)
-      (gimp-palette-set-foreground old-fg)
-      (gimp-image-enable-undo img)
-      (gimp-display-new img))))
+(define (apply-luminous-logo-effect img
+                                    logo-layer
+                                    feather
+                                    luminous-color
+                                    grow
+                                    background-color)
+  (let* ((width (car (gimp-drawable-width logo-layer)))
+         (height (car (gimp-drawable-height logo-layer)))
+         (bg-layer (car (gimp-layer-new img
+                                        width height RGB-IMAGE
+                                        "Background" 100 NORMAL-MODE)))
+         (luminous-layer (car (gimp-layer-new img
+                                              width height RGBA-IMAGE
+                                              "Luminous" 100 NORMAL-MODE))))
+    (gimp-context-push)
+    (gimp-context-set-defaults)
+    (gimp-selection-none img)
+    (script-fu-util-image-resize-from-layer img logo-layer)
+    (script-fu-util-image-add-layers img luminous-layer bg-layer)
+    (gimp-layer-set-lock-alpha logo-layer TRUE)
+    (gimp-context-set-background background-color)
+    (gimp-edit-fill bg-layer BACKGROUND-FILL)
+    (gimp-edit-clear luminous-layer)
+    (gimp-image-select-item img CHANNEL-OP-REPLACE logo-layer)
+    (gimp-selection-grow img grow)
+    (gimp-selection-feather img feather)
+    (gimp-context-set-foreground luminous-color)
+    (gimp-edit-fill luminous-layer FOREGROUND-FILL)
+    (gimp-selection-none img)
+    (gimp-context-pop)))
 
+(define (script-fu-luminous-logo-alpha img
+                                       logo-layer
+                                       luminous-color
+                                       feather
+                                       grow
+                                       background-color)
+  (begin
+    (gimp-image-undo-group-start img)
+    (apply-luminous-logo-effect img logo-layer feather luminous-color grow background-color)
+    (gimp-image-undo-group-end img)
+    (gimp-displays-flush)))
+
+(script-fu-register "script-fu-luminous-logo-alpha"
+                    _"Luminous..."
+                    _"Add a luminous effect around the selected region (or alpha)"
+                    "Takashi P. KATO(H)"
+                    "Takashi P. KATO(H)"
+                    "1998"
+                    "RGBA"
+                    SF-IMAGE       "Image"          0
+                    SF-DRAWABLE    "Drawable"       0
+                    SF-COLOR      _"Luminous color" '(63 252 0)
+                    SF-ADJUSTMENT _"Feather size"   '(50 2 1000 1 10 0 1)
+                    SF-ADJUSTMENT _"Grow steps"     '(10 2 1000 1 10 0 1)
+                    SF-COLOR      _"Background"     (car (gimp-palette-get-background)))
+
+(script-fu-menu-register "script-fu-luminous-logo-alpha"
+                         "<Image>/Filters/Alpha to Logo")
+
+(define (script-fu-luminous-logo text
+                                 font
+                                 size
+                                 foreground-color
+                                 background-color
+                                 luminous-color
+				 feather
+                                 grow)
+  (gimp-context-push)
+  (gimp-context-set-foreground foreground-color)
+  (let* ((img (car (gimp-image-new 256 256 RGB)))
+         (border (+ feather grow))
+         (text-layer (car (gimp-text-fontname img
+                                              -1 0 0 text border TRUE
+                                              size PIXELS font)))
+         (width (car (gimp-drawable-width text-layer)))
+         (height (car (gimp-drawable-height text-layer))))
+    (gimp-image-undo-disable img)
+    (apply-luminous-logo-effect img text-layer feather luminous-color grow background-color)
+    (gimp-image-undo-enable img)
+    (gimp-display-new img))
+  (gimp-context-pop))
 
 (script-fu-register "script-fu-luminous-logo"
-		    "<Toolbox>/Xtns/Script-Fu/Logos/Luminous"
-		    "Create luminescent logo with the specified glow color"
-		    "Takashi P.KATOH"
-		    "Takashi P.KATOH"
-		    "1998"
-		    ""
-		    SF-VALUE "Text String" "\"Luminous \""
-		    SF-VALUE "Font Size (in pixels)" "150"
-		    SF-VALUE "Font" "\"utopia\""
-		    SF-COLOR "Glow Color" '(63 252 0)
-		    SF-VALUE "Grow strength" "10"
-		    SF-COLOR "Background" '(0 0 0))
+                    _"Luminous..."
+                    _"Create a logo with a luminous text effect"
+                    "Takashi P. KATO(H)"
+                    "Takashi P. KATO(H)"
+                    "1998"
+                    ""
+                    SF-STRING     _"Text"               "Luminous"
+                    SF-FONT       _"Font"               "Sans Bold"
+                    SF-ADJUSTMENT _"Font size (pixels)" '(100 2 1000 1 10 0 1)
+                    SF-COLOR      _"Text color"         (car (gimp-palette-get-foreground))
+                    SF-COLOR      _"Background"         (car (gimp-palette-get-background))
+                    SF-COLOR      _"Luminous color"     '(63 252 0)
+                    SF-ADJUSTMENT _"Feather size"       '(50 2 1000 1 10 0 1)
+                    SF-ADJUSTMENT _"Grow steps"         '(10 2 1000 1 10 0 1))
+
+(script-fu-menu-register "script-fu-luminous-logo"
+                         "<Image>/File/Create/Logos")
